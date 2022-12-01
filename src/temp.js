af58c8bd5e6424eeac66d437bf9c0343490e1827
olegpl1993 <olegpl1993@gmail.com>
2022-12-01 21:17:14 +0200

    pass 26

--- src/temp.js
+++ src/temp.js
@@ -1,24 +1,10 @@
 /*
-function roundToPowerOfTen(num, pow) {
-  let number;
-  if (pow === 0) {
-    number = num;
-  } else if (pow === 1) {
-    number = Math.round(num / 10) * 10;
-  } else if (pow === 2) {
-    number = Math.round(num / 100) * 100;
-  } else {
-    number = Math.round(num / 1000) * 1000;
-  }
-  return number;
+function generateOdds(len) {
+  const arr = new Array(len).fill(0).map((val, index) => index * 2 + 1);
+  return arr;
 }
 
-console.log(roundToPowerOfTen(1234, 0)); // 1234);
-console.log(roundToPowerOfTen(1234, 1)); // 1230);
-console.log(roundToPowerOfTen(1234, 2)); // 1200);
-console.log(roundToPowerOfTen(1234, 3)); // 1000);
-console.log(roundToPowerOfTen(9678, 0)); // 9678);
-console.log(roundToPowerOfTen(9678, 1)); // 9680);
-console.log(roundToPowerOfTen(9678, 2)); // 9700);
-console.log(roundToPowerOfTen(9678, 3)); // 10000);
+console.log(generateOdds(1)); // => [ 1 ]
+console.log(generateOdds(2)); // => [ 1, 3 ]
+console.log(generateOdds(5)); // => [ 1, 3, 5, 7, 9 ]
 */
