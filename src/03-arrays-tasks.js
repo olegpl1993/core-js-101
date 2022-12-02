8d0be0c547566bc92732c32cd05b575d6bf973c1
olegpl1993 <olegpl1993@gmail.com>
2022-12-02 20:30:52 +0200

    pass 43

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -339,7 +339,6 @@ function sortDigitNamesByNumericOrder(/* arr */) {
  */
 function getItemsSum(arr) {
   return arr.reduce((acc, val) => acc + val, 0);
-  // throw new Error('Not implemented');
 }
 
 /**
@@ -354,8 +353,8 @@ function getItemsSum(arr) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.filter((el) => !(el)).length;
 }
 
 /**
