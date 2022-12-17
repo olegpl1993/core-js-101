5beb801ff40ed0094103458cbb4eca57fc5db6cd
olegpl1993 <olegpl1993@gmail.com>
2022-12-17 20:51:39 +0200

    pass 52

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -569,8 +569,12 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  return arr.length % 2 === 0
+    ? [arr.slice(arr.length / 2, arr.length), arr.slice(0, Math.ceil(arr.length / 2))].flat(1)
+    : [arr.slice(Math.ceil(arr.length / 2), arr.length),
+      arr[Math.floor(arr.length / 2)],
+      arr.slice(0, (arr.length / 2))].flat(1);
 }
 
 
--- src/temp.js
+++ src/temp.js
@@ -1,14 +1,16 @@
 /*
-function getElementByIndexes(arr, indexes) {
-  if (indexes.length === 1) return arr[indexes[0]];
-  if (indexes.length === 2) return arr[indexes[0]][indexes[1]];
-  if (indexes.length === 3) return arr[indexes[0]][indexes[1]][indexes[2]];
+function swapHeadAndTail(arr) {
+  return arr.length % 2 === 0
+    ? [arr.slice(arr.length / 2, arr.length), arr.slice(0, Math.ceil(arr.length / 2))].flat(1)
+    : [arr.slice(Math.ceil(arr.length / 2), arr.length),
+      arr[Math.floor(arr.length / 2)],
+      arr.slice(0, (arr.length / 2))].flat(1);
 }
 
-console.log(getElementByIndexes([[1, 2], [3, 4], [5, 6]], [0, 0]));
-//  => 1        (arr[0][0])
-console.log(getElementByIndexes(['one', 'two', 'three'], [2]));
-//       => 'three'  (arr[2])
-console.log(getElementByIndexes([[[1, 2, 3]]], [0, 0, 1]));
-//      => 2        (arr[0][0][1])
+console.log(swapHeadAndTail([1, 2, 3, 4, 5]));
+//   =>  [ 4, 5, 3, 1, 2 ]
+console.log(swapHeadAndTail([1, 2]));
+//  => [ 2, 1 ]
+console.log(swapHeadAndTail([1, 2, 3, 4, 5, 6, 7, 8]));
+//   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
 */
