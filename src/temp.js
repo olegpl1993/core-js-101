a442f9d64e088afd3bcea1d160b3923f425aa250
olegpl1993 <olegpl1993@gmail.com>
2022-12-24 01:23:12 +0200

    pass 61

--- src/temp.js
+++ src/temp.js
@@ -1,10 +1,12 @@
 /*
-function timeSpanToString(startDate, endDate) {
-  return new Date(endDate - startDate).toISOString().slice(11, -1);
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  this.getArea = () => width * height;
 }
 
-console.log(timeSpanToString(
-  new Date(2000, 1, 1, 10, 0, 0), new Date(2000, 1, 1, 15, 20, 10, 453),
-));
-//   => "05:20:10.453"
+const r = new Rectangle(10, 20);
+console.log(r.width); // => 10
+console.log(r.height); // => 20
+console.log(r.getArea()); // => 200
 */
